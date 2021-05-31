1f962554447d45e3dbac626de9ca21ff8d9a0675
Codie Newark <git@codie.gg>
2021-05-31 17:42:52 +0100

    New site.

--- script.js
+++
@@ -1,7 +0,0 @@
-
-// Background image selection
-// const backgroundImageCount = 6;
-
-// var backgroundImageElement = document.getElementById("backgroundImage");
-// var randomImage = Math.random() * (backgroundImageCount - 1) + 1;
-// backgroundImageElement.style.setProperty("background-image", `url(./img/backgrounds/${randomImage.toFixed(0)}.jpg)`)
